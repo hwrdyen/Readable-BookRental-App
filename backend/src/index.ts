43bc07c2a0fc8a10e21257f47238d806235bedd0
hwrdyen <hwrdpvt@gmail.com>
2024-01-19 21:30:26 -0500

    Setup backend index.ts file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import cors from "cors";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req: Request, res: Response) => {
   return res.status(200).send("Express Home");
